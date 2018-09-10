768254beccc8f41dac499e3063d6b4c30ab24f09
Mateusz Chwedczuk <mateusz.chwedczuk@insanelab.com>
2018-09-11 00:16:21 +0200

    Initial

---
+++ SignalrServer/Hubs/SimpleHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrServer.Api.Hubs
+{
+    public class SimpleHub : Hub
+    {
+        public SimpleHub()
+        {
+
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task GetTestResponse()
+        {
+            await Clients.Caller.SendAsync("TestResponse", "some text");
+        }
+    }
+}
---
+++ SignalrServer/Hubs/UsersHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRServer.Services.ConnectionStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrServer.Api.Hubs
+{
+    public class UsersHub : Hub
+    {
+        private readonly IConnectionStore _store;
+
+        public UsersHub(IConnectionStore store)
+        {
+            _store = store;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //_store.Add(new ConnectedUser(Context.ConnectionId, userId));
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
